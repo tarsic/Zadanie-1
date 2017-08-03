fab312f15b3a484573d3569fc98c231e760d8931
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-03 14:24:35 -0500

    New class created
    Zadanie vypolneno!

--- create_contact.py
+++ create_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from contact import contact
 
 def is_alert_present(wd):
     try:
@@ -13,47 +13,65 @@ def is_alert_present(wd):
 class create_contact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(60)  
     
     def test_create_contact(self):
-        # open home page
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        # create new contact
-        wd.find_element_by_link_text("add new").click()
+        wd = self.open_home_page()
+        self.login(wd)
+        self.create_new_contact(wd)
+        self.fill_out_form(wd, contact(firstname="Romn", middlename="M", lastname="Smith", nickname="Pet", company="Ford", address="Chicago"))
+        self.submit_creating(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def submit_creating(self, wd):
+        # submit creating
+        wd.find_element_by_name("submit").click()
+
+    def fill_out_form(self, wd, contact):
         # fill out the form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Romn")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("M")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Smith")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("pet")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("ford")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("chicago")
-        # submit creating
-        wd.find_element_by_name("submit").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-        
-    
+        wd.find_element_by_name("address").send_keys(contact.address)
+
+    def create_new_contact(self, wd):
+        # create new contact
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username="admin", password="secret"):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self):
+        # open home page
+        wd = self.wd
+        wd.get("http://localhost/addressbook/index.php")
+        return wd
+
     def tearDown(self):
         self.wd.quit()
 
