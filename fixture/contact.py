e665d9599013de391ff04434fb9853f70c64819d
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-08 14:00:37 -0500

    edit_contact test created

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,6 +8,34 @@ class ContactHelper:
         # create new contact
         wd.find_element_by_link_text("add new").click()
 
+    def edit_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+
+
+    def fill_edit_form(self, contact):
+        wd = self.app.wd
+        self.edit_contact()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("update").click()
+
     def fill_out_form(self, contact):
         wd = self.app.wd
         self.create_new_contact()
