ea742ba63960de8108d2b0d505e90e5d2e1a6ca3
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-08 09:54:51 -0500

    Delete_first_group test created.
    Conftest file created.

--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,15 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         self.submit_group_creation()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        wd.find_element_by_link_text("group page").click()
+
     def submit_group_creation(self):
         wd = self.app.wd
         wd.find_element_by_name("submit").click()
