658dfcbe565949cf33757c21297f516f55669e82
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-11 09:28:38 -0500

    test_edit_group file updated with if_then statements

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,11 @@
 from model.group import group
 
-def test_edit_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_group(group(name="adeptissimo", header="moreover", footer="learningpy"))
+    app.group.edit_first_group(group(name="New name"))
+    app.session.logout()
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(group(header="New header"))
     app.session.logout()
