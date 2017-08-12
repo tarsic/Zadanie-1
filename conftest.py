3581bf1e269f5ed8b29f0b2f62551a40d21727b9
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-11 20:04:03 -0500

    login and logout enchanced
    check-points added

--- conftest.py
+++ conftest.py
@@ -10,11 +10,10 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
@@ -22,7 +21,7 @@ def app(request):
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
 
--- fixture/application.py
+++ fixture/application.py
@@ -8,10 +8,16 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/index.php")
 
     def is_valid(self):
         try:
@@ -19,10 +25,6 @@ class Application:
             return True
         except:
             return False
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
