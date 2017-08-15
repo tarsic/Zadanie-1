e898b4f7efef516f4498d6d8bee340c1d5178231
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-15 11:07:17 -0500

    Precondition to edit_grouop added.
    Now all delete and modify tests have precondition checking.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,11 @@
 from model.group import group
 
 def test_edit_group_name(app):
+    if app.group.count () == 0:
+        app.group.fill_group_form(group(name="test"))
     app.group.edit_first_group(group(name="New name"))
 
 def test_edit_group_header(app):
+    if app.group.count () == 0:
+        app.group.fill_group_form(group(name="gut", header="test"))
     app.group.edit_first_group(group(header="New header"))
