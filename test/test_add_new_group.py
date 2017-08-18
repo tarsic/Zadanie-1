ef8ab74a5be0d7d41933fd68e2420492ccbc5f8c
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-18 13:56:32 -0500

    Verification of creation added to add_group

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -7,11 +7,20 @@ from model.group import group
 
 
 def test_add_new_group(app):
+    old_groups = app.group.get_group_list()
     app.group.fill_group_form(group(name="adept", header="more", footer="learning"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.fill_group_form(group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
+
 
 
 
