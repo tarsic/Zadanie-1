6ce211103e2a791e57ea5ef5c340d17efee44a0d
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-18 14:05:55 -0500

    Verification added to edit_group_name

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,5 +3,8 @@ from model.group import group
 def test_delete_first_group(app):
     if app.group.count () == 0:
         app.group.fill_group_form(group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,9 +3,16 @@ from model.group import group
 def test_edit_group_name(app):
     if app.group.count () == 0:
         app.group.fill_group_form(group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(group(name="New name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count () == 0:
         app.group.fill_group_form(group(name="gut", header="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
