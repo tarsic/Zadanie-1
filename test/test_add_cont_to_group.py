2d290167fa107f2a7c4bab9f72b66cd9d201c9c8
tarsic <30630412+tarsic@users.noreply.github.com>
2017-08-25 10:16:46 -0500

    Len assert added to del_contact

--- test/test_add_cont_to_group.py
+++ test/test_add_cont_to_group.py
@@ -1,3 +1,7 @@
+from fixture.contact import Contact
 
 def test_add_cont_to_group(app):
+    if app.contact.count() == 0:
+        app.contact.fill_out_form(Contact(firstname="Romn", middlename="M", lastname="Smith", nickname="Pet",
+                                          company="Ford", address="Chicago"))
     app.contact.add_cont_to_group()
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,5 +2,13 @@ from model.contact import Contact
 
 def test_del_contact(app):
     if app.contact.count() == 0:
-        app.contact.fill_out_form(Contact(firstname="Romn", middlename="M", lastname="Smith", nickname="Pet", company="Ford", address="Chicago"))
+        app.contact.fill_out_form(Contact(firstname="Romn", middlename="M", lastname="Smith", nickname="Pet",
+                                          company="Ford", address="Chicago"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+
+
+
+
