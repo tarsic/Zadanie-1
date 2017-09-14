d038200e39c0f13ec278fe52c6d571954c681f92
tarsic <30630412+tarsic@users.noreply.github.com>
2017-09-14 17:27:40 -0500

    ui changed for db in add and del group tests

--- fixture/group.py
+++ fixture/group.py
@@ -39,6 +39,10 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def delete_first_group(self):
         self.delete_group_by_index(0)
         self.group_cache = None
@@ -51,6 +55,17 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         wd.find_element_by_link_text("home").click()
         self.group_cache = None
+
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        wd.find_element_by_link_text("home").click()
+        self.group_cache = None
+
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -105,3 +120,4 @@ class GroupHelper:
                 id=element.find_element_by_name("selected[]").get_attribute("value")
                 self.group_cache.append(Group(name=text, id=id))
         return list(self.group_cache)
+
