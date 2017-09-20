170cdc34003e7fafc5cb095bba1ae2bbf70ede08
tarsic <30630412+tarsic@users.noreply.github.com>
2017-09-20 13:48:37 -0500

    create contact db assert added

--- fixture/db.py
+++ fixture/db.py
@@ -27,10 +27,10 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address))
         finally:
             cursor.close()
         return list
--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -9,7 +9,7 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*5
+    symbols = string.ascii_letters + string.digits + " "*2
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -27,15 +27,15 @@ testdata = [
             workphone=some_phone3, email=some_email, email2=some_email2, email3=some_email3,
             secondaryphone=some_phone4, nickname=random_string("nick", 20),
             company=random_string("comp", 20), address=random_string("addr", 20))
-    for i in range(2)
+    for i in range(1)
 ]
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_create_contact(app, contact):
-    old_contacts = app.contact.get_contact_list()
+def test_create_contact(app, db, contact):
+    old_contacts = db.get_contact_list()
     app.contact.fill_out_form(contact)
     assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
