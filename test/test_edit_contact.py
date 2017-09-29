899b70f5bed0186f9cd2490d69deb1c759a23366
tarsic <30630412+tarsic@users.noreply.github.com>
2017-09-29 07:44:52 -0500

    check_ui added to edit_contact

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 import random
 
-def test_edit_some_contact(app, db):
+def test_edit_some_contact(app, db, check_ui):
     if app.contact.count() == 0:
         app.contact.fill_out_form(Contact(firstname="Romn", middlename="M", lastname="Smith", nickname="Pet",
                                           company="Ford", address="Chicago"))
@@ -16,3 +16,11 @@ def test_edit_some_contact(app, db):
     assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = contact_now
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        #def clean(contact):
+           # return Contact(id=contact_was.id, firstname=contact.firstname.strip())
+       # new_contacts = list(map(clean, db.get_contact_list()))
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+
+
+
